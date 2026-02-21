9876835d1d32820cfb20ad9f82132a445387654d
Nicolás Carrera <nicolascarrera42@gmail.com>
2026-02-21 12:38:58 -0500

    feat: base project

---
+++ src/test/java/com/assetguard/core/AssetGuardCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assetguard.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetGuardCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
